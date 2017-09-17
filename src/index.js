b750b4ba2c4604021683206ba3ae7d1f1f5777b4
Kim Seungha <seungha.me@gmail.com>
2017-09-17 16:28:39 +0900

    trust proxy

--- src/index.js
+++ src/index.js
@@ -26,6 +26,7 @@ const sessionMiddleware = cookieSession({
   ]
 })
 app.set('view engine', 'pug')
+app.set('trust proxy')
 
 app.use(express.static(path.join(__dirname, '..', 'public')))
 app.use(bodyParser.urlencoded({extended: false}))
