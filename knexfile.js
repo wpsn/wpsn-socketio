93297191783546f793a562cf047ae2b471730936
Kim Seungha <seungha.me@gmail.com>
2017-09-17 17:06:17 +0900

    polish

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,17 @@
 require('dotenv').config()
+const common = {
+  client: 'mysql',
+  connection: {
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME
+  }
+}
 
 module.exports = {
-  development: {
-    client: 'mysql',
-    connection: {
-      host: process.env.DATABASE_HOST,
-      user: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME
-    },
+  development: Object.assign({}, common, {
     debug: true
-  }
+  }),
+  production: common
 };
